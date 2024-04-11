19fb1e6e7a4c47374a4c054bffa1b46ffc292c9b
santiagoaloi <santiagoaloi@gmail.com>
2024-04-11 11:23:08 +0200

    hide logo to make space to action buttons in small viewports

--- src/components/app-bar/GAppBar.vue
+++ src/components/app-bar/GAppBar.vue
@@ -1,18 +1,24 @@
+<script setup>
+const { smAndDown } = useDisplay()
+</script>
+
 <template>
   <VAppBar
     :height="100"
-    class=" px-3 " color="background"
+    class="px-3 " color="background"
   >
     <VCard
-      :style="`width: ${1450}px`" class="mx-auto flex items-center p-7 mt-2 elevation-0 dark:backdrop-blur"
+      :style="`width: ${1450}px`" class="mx-auto flex items-center p-7 mt-2 elevation-0 d"
       color="app-background" height="68"
     >
-      <KiveLogo class="ml-5" />
+      <template v-if="!smAndDown">
+        <KiveLogo class="ml-5" />
 
-      <div class="text-lg">
-        XKCD Comics
-      </div>
-      <VSpacer />
+        <div class="text-lg">
+          XKCD Comics
+        </div>
+        <VSpacer />
+      </template>
 
       <ComicsActionButtons />
     </VCard>
