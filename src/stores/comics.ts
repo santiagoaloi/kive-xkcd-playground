9c11f7b392981bf7e31225f3f0db1cbe5216953f
santiagoaloi <santiagoaloi@gmail.com>
2024-04-11 10:12:22 +0200

    adding types to state

--- src/stores/comics.ts
+++ src/stores/comics.ts
@@ -1,12 +1,37 @@
 import { defineStore } from 'pinia'
 
+interface Comic {
+  month: string
+  num: number
+  link: string
+  year: string
+  news: string
+  safe_title: string
+  transcript: string
+  alt: string
+  img: string
+  title: string
+  day: string
+}
+
+interface ComicsState {
+  comics: {
+    current: Comic
+    gallery: Comic[]
+  }
+  currentComic: number | null
+  mostRecentComicId: number | null
+  loading: boolean
+}
+
 export const useComicsStore = defineStore('comics-store', {
-  state: () => ({
+  state: (): ComicsState => ({
     comics: {
-      current: {},
+      current: {} as Comic,
       gallery: [],
     },
     currentComic: null,
+    mostRecentComicId: null,
     loading: false,
   }),
 
@@ -15,6 +40,35 @@ export const useComicsStore = defineStore('comics-store', {
   },
 
   actions: {
+
+    /*
+ * Get me the most recent comic id.
+ * we can use it to fallback to this comic if the user
+ * routes to a comic that doesn't exist and also to prevent the user
+ * from going to a comic that doesn't exist.
+ */
+
+    async getMostRecentComicId() {
+      const url = import.meta.env.DEV
+        ? `/api/info.0.json`
+        : `/api/xkcd?id`
+
+      try {
+        const { data: comicData } = await axios(url)
+        this.comics.current = comicData
+        this.mostRecentComicId = comicData.num
+      }
+      catch (error) {
+        console.error('Error fetching comic:', error)
+      }
+      finally {
+        this.loading = false
+      }
+    },
+
+    /*
+ * Handle all the logic for fetching the comics with the action buttons.
+ */
     async switchComic(id: number) {
       const url = import.meta.env.DEV
         ? `/api/${id}/info.0.json`
