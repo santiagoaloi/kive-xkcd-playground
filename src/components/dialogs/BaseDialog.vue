51a2172cc6affd92de431a6ddecfd7287fe8d9fe
santiagoaloi <santiagoaloi@gmail.com>
2024-04-11 16:09:32 +0200

    added re-usable base dialog

---
+++ src/components/dialogs/BaseDialog.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    default: 'title',
+  },
+
+})
+
+const dialog = defineModel()
+</script>
+
+<template>
+  <VDialog v-model="dialog" max-width="500" transition="fade-transition">
+    <VCard>
+      <VCardText>
+        <div class="text-h5 mb-4">
+          <b> {{ title }}</b>
+        </div>
+
+        <slot />
+      </VCardText>
+
+      <VCardActions>
+        <VSpacer />
+
+        <VBtn
+          color="pink"
+          text="Close"
+          @click="dialog = false"
+        />
+      </VCardActions>
+    </VCard>
+  </VDialog>
+</template>
