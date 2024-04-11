2708c389a544af6dfe6c9ba7a8525d15b3b4c373
santiagoaloi <santiagoaloi@gmail.com>
2024-04-11 14:23:10 +0200

    added 404 page

---
+++ src/pages/[...404].vue
@@ -0,0 +1,24 @@
+<template>
+  <VContainer class="flex align-center justify-center  h-100">
+    <div class="align-center justify-center flex flex-col">
+      <div>
+        <Duo404 size="190" />
+      </div>
+
+      <div class="my-10">
+        The page <b>{{ $route.path }} </b> does not exist.
+      </div>
+
+      <SettingsButton
+        class="rounded-xl w-100 sm:w-auto"
+        color="black-button"
+        light
+        to="/"
+      >
+        <div>
+          Keep exploring comics!
+        </div>
+      </SettingsButton>
+    </div>
+  </VContainer>
+</template>
