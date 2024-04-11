0a34cf581c5e1179ba52e6e77b2f5e45f0401320
santiagoaloi <santiagoaloi@gmail.com>
2024-04-11 14:23:19 +0200

    added not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <VContainer class="flex align-center justify-center  h-100">
+    <div class="align-center justify-center flex flex-col">
+      <div>
+        <DuoNoresults size="190" />
+      </div>
+
+      <div class="my-10">
+        Ths comic is not found
+      </div>
+
+      <SettingsButton
+        class="rounded-xl w-100 sm:w-auto"
+        color="black-button"
+        light
+        to="/"
+      >
+        <div>
+          Keep exploring comics!
+        </div>
+      </SettingsButton>
+    </div>
+  </VContainer>
+</template>
