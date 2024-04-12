6b2b9574ded81f3715726a95dc7aa0307f7f4c91
santiagoaloi <santiagoaloi@gmail.com>
2024-04-12 10:44:46 +0200

    removed irrelevant comments

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,3 @@
-/**
- * router/index.ts
- *
- * Automatic routes for `./src/pages/*.vue`
- */
-
-// Composables
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
 
