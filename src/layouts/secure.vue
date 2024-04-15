bd0e15250d80ecbcfd1b8ffe16d7b92a81da66b2
santiagoaloi <santiagoaloi@gmail.com>
2024-04-15 13:47:12 +0200

    render secure layout only when the profile is loaded

--- src/layouts/secure.vue
+++ src/layouts/secure.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+import { useUserProfileStore } from '@/stores/user-profile'
+
+const profileStore = useUserProfileStore()
+</script>
+
 <template>
-  <VMain>
+  <!-- Only render this layout if the user profile is acutally loaded -->
+  <VMain v-if="profileStore.profileId">
     <RouterView />
   </VMain>
 </template>
