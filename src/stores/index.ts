eb519850f910f6a722a70e6cc90b8606e4c26e93
santiagoaloi <santiagoaloi@gmail.com>
2024-04-15 12:05:19 +0200

    allow the usage of router in pinia stores

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,4 +1,14 @@
 // Utilities
 import { createPinia } from 'pinia'
+import { markRaw } from 'vue'
+import router from '@/router'
 
-export default createPinia()
+const pinia = createPinia()
+
+pinia.use(({ store }) => {
+  // The 'router' instance is marked as raw using 'markRaw'.
+  // This prevents Vue from observing and tracking it in the reactivity system.
+  store.router = markRaw(router)
+})
+
+export default pinia
