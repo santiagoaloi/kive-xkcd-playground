e9d911f54430dd77a9cfedcfc3f94e993e311ab1
santiagoaloi <santiagoaloi@gmail.com>
2024-04-16 01:42:31 +0200

    added vuefire logic to plugins

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,8 +1,11 @@
 // Plugins
 import type { App } from 'vue'
+import { VueFire, VueFireFirestoreOptionsAPI } from 'vuefire'
+
 import pinia from '../stores'
 import router from '../router'
 import vuetify from './vuetify'
+import { firebaseApp } from '@/firebase'
 
 // Types
 
@@ -10,5 +13,9 @@ export function registerPlugins(app: App) {
   app
     .use(vuetify)
     .use(router)
+    .use(VueFire, {
+      firebaseApp,
+      modules: [VueFireFirestoreOptionsAPI()],
+    })
     .use(pinia)
 }
