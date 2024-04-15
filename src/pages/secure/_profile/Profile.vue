676dc75393a6c98b0b985696a721e0f100ddab3d
santiagoaloi <santiagoaloi@gmail.com>
2024-04-16 01:42:56 +0200

    added secure profile componet

--- src/pages/secure/_profile/Profile.vue
+++ src/pages/secure/_profile/Profile.vue
@@ -8,7 +8,7 @@ const profileStore = useUserProfileStore()
   <div class="flex">
     <div class="py-5 border-b pl-3 bg-redx gap-y-2 flex flex-col bg-bluex w-100 text-black dark:text-[#ccc]">
       <div class="text-3xl ">
-        {{ profileStore.profileFirstName }}'s Dashboard
+        {{ profileStore.profileFirstName }}'s Profile
       </div>
 
       <div>
@@ -18,45 +18,13 @@ const profileStore = useUserProfileStore()
   </div>
 
   <div class="h-full grid grid-cols-12 gap-6 pt-5">
-    <div class="lg:block hidden col-span-2">
+    <div class="lg:block hidden col-span-3">
       <div class="sticky top-24">
         <ProfileDrawer />
       </div>
     </div>
 
     <div class="col-span-12 lg:col-span-9">
-      <VSheet class="sticky top-20 align-center" color="background" height="80">
-        <div class="flex flex-col h-100 w-100 align-center justify-center">
-          <div class="w-100 flex align-center  bg-greenx">
-            <VSpacer />
-
-            <SettingsButton class="mr-1" height="40">
-              Search
-            </SettingsButton>
-
-            <SettingsButton append-icon="i-mdi:chevron-down" class="mr-1" height="40" light>
-              Sort by relevance
-            </SettingsButton>
-
-            <VBtnToggle
-              class=" rounded-lg"
-              density="compact"
-              divided
-              mandatory
-              style="height: 40px"
-              variant="outlined"
-            >
-              <VBtn>
-                <VIcon icon="i-mdi-format-list-bulleted-square" />
-              </VBtn>
-              <VBtn>
-                <VIcon icon="i-mdi-dots-grid" />
-              </VBtn>
-            </VBtnToggle>
-          </div>
-        </div>
-      </VSheet>
-
       <div>
         <RouterView />
       </div>
