97478bab95467c150b23193e00f859759c71511b
santiagoaloi <santiagoaloi@gmail.com>
2024-04-19 00:04:16 +0200

    Refactor Firebase service to include TSdoc

--- src/services/firebase-service.ts
+++ src/services/firebase-service.ts
@@ -4,6 +4,7 @@
 export interface Options {
   /**
    * The name of the field to update. If this is not provided, the entire document will be updated with the payload.
+   * @default undefined
    */
   fieldName?: string
 
@@ -11,26 +12,32 @@ export interface Options {
    * The toggle option allows you to add or remove items from an array field in a document.
    * If an item in the payload already exists in the array (based on the key), it will be removed.
    * If it doesn't exist, it will be added.
-   *
    * The toggle option should be an object with a single property: key.
    * The key is the property of the items in the array to use for comparison.
-   *
-   * Example: { key: 'id' }
+   * @example { key: 'id' }
+   * @default undefined
    */
   toggle?: { key: string }
 
   /**
    * The dedup option allows you to add items from the payload to an array field in a document without adding duplicates.
    * If an item in the payload already exists in the array (based on the key), it will not be added.
-   *
    * The dedup option should be an object with a single property: key.
    * The key is the property of the items in the array to use for comparison.
-   *
-   * Example: { key: 'id' }
+   * @example { key: 'id' }
+   * @default undefined
    */
   dedup?: { key: string }
 }
 
+/**
+ * Updates a document in a Firebase collection.
+ * @param collectionName - The name of the collection.
+ * @param documentId - The ID of the document to update.
+ * @param payload - The data to update the document with.
+ * @param options - Options for how to update the document.
+ * @returns A promise that resolves when the update is complete.
+ */
 export async function updateDocumentService(
   collectionName: string,
   documentId: string,
@@ -74,9 +81,17 @@ export async function updateDocumentService(
   await updateDoc(documentRef, payloadObject)
 }
 
+/**
+ * Toggles items in an array based on a key.
+ * @param existingArray - The existing array.
+ * @param payload - The items to toggle.
+ * @param key - The key to use for comparison.
+ * @returns The updated array.
+ */
 function toggleItems(existingArray: any[], payload: any[], key: string): any[] {
   return payload.reduce((acc, item) => {
     const index = acc.findIndex(existingItem => existingItem[key] === item[key])
+
     if (index !== -1) {
       // item exists, remove it
       return [...acc.slice(0, index), ...acc.slice(index + 1)]
@@ -88,6 +103,13 @@ function toggleItems(existingArray: any[], payload: any[], key: string): any[] {
   }, existingArray)
 }
 
+/**
+ * Adds items to an array without adding duplicates.
+ * @param existingArray - The existing array.
+ * @param payload - The items to add.
+ * @param key - The key to use for comparison.
+ * @returns The updated array.
+ */
 function dedupItems(existingArray: any[], payload: any[], key: string): any[] {
   return [...existingArray, ...payload.filter(item => !existingArray.some(existingItem => existingItem[key] === item[key]))]
 }
