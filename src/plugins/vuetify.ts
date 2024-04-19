341bc5bba7cc8804fd06a95bc020fb259fdc9cf0
santiagoaloi <santiagoaloi@gmail.com>
2024-04-20 00:21:16 +0200

    remove irrelevant comments

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,16 +1,12 @@
 import 'vuetify/styles'
-
-// Composables
 import { createVuetify } from 'vuetify'
 
-// Import default configurations, theme, and icons from '@/config/vuetify'.
 import {
   defaultsCustom,
   defaultsNative,
   themes,
 } from '../config/vuetify'
 
-// Create a new Vuetify instance with custom configurations.
 export default createVuetify({
 
   theme: {
