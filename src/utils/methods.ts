ec8ac9afeb545ea92870133b9ab5f3cf75a7f11c
santiagoaloi <santiagoaloi@gmail.com>
2024-04-20 15:58:55 +0200

    avoid displaying uppercase words, force them to lowercase

--- src/utils/methods.ts
+++ src/utils/methods.ts
@@ -2,18 +2,15 @@
  * Capitalizes the first letter of the input string.
  *
  * @param str - The string to capitalize.
- * @returns The input string with the first letter capitalized, or undefined if the input string is falsy.
+ * @param lowerRest - Optional. Whether to convert the rest of the string to lowercase. Defaults to true.
+ * @returns The input string with the first letter capitalized and the rest of the string optionally converted to lowercase,
+ * or undefined if the input string is falsy.
  */
-export function capitalize(str: string): string | undefined {
-  // If the input string is empty or falsy (e.g., null, undefined, empty string),
-  // return 'undefined'.
+export function capitalize(str: string, lowerRest: boolean = true): string | undefined {
   if (!str)
     return
 
-  // Capitalize the first letter of the input string by combining:
-  // 1. The first character of the input string converted to uppercase.
-  // 2. The rest of the input string (excluding the first character).
-  return str[0].toUpperCase() + str.slice(1)
+  return str[0].toUpperCase() + (lowerRest ? str.slice(1).toLowerCase() : str.slice(1))
 }
 
 /**
@@ -24,11 +21,7 @@ export function capitalize(str: string): string | undefined {
  * @returns The converted sentence.
  */
 export function camelCaseToSentence(camelCase: string): string {
-  // Add a space before all uppercase letters, then make the entire string lowercase
   let sentence: string = camelCase.replace(/([A-Z])/g, ' $1').toLowerCase()
-
-  // Capitalize the first letter
   sentence = sentence.charAt(0).toUpperCase() + sentence.slice(1)
-
   return sentence
 }
