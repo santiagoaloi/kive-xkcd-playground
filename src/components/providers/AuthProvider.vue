29a51553684bdf307cff36f899d562f2e9631b42
santiagoaloi <santiagoaloi@gmail.com>
2024-04-20 15:40:38 +0200

    improved UX by making sure the app is render after authentication is done

--- src/components/providers/AuthProvider.vue
+++ src/components/providers/AuthProvider.vue
@@ -1,11 +1,20 @@
-<script setup>
+<script setup lang="ts">
 import { setUserAndProfile } from '@/firebase/authentication'
 
+const loading = ref(true)
+
+// This listener is triggered when the user signs in or out, and also right after the listener is registered
 auth.onAuthStateChanged(async (user) => {
+  // Update the application's state with the user's information
   await setUserAndProfile(user)
+
+  // Once the user's information has been set, update the loading state to false
+  // This will happy only on the first load of the application.
+  if (loading.value)
+    loading.value = false
 })
 </script>
 
 <template>
-  <slot />
+  <slot v-if="!loading" />
 </template>
