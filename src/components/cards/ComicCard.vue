4a4daf931a4eef87d6af12f5cd74d64a3ed298bc
santiagoaloi <santiagoaloi@gmail.com>
2024-04-21 02:53:27 +0200

    add hover buttons in comicCard component

--- src/components/cards/ComicCard.vue
+++ src/components/cards/ComicCard.vue
@@ -27,48 +27,64 @@ const emit = defineEmits('unfavorite', 'clickLink')
 
 <template>
   <div>
-    <VCard class="active-push-down rounded-lg flex flex-col h-full card-color">
-      <div>
-        <VImg
-          :src="img"
-          class="w-full prevent-select image-hover-scalex bg-background border-b"
+    <VHover v-slot="{ isHovering, props }">
+      <VCard
+        v-bind="props"
+        class="active-push-down rounded-lg flex flex-col h-full card-color"
+      >
+        <div>
+          <VImg
+            :src="img"
+            class="w-full prevent-select image-hover-scalex bg-background border-b"
 
-          height="320"
-        >
-          <template #placeholder>
-            <div class="flex h-full items-center justify-center">
-              <VProgressCircular color="primary" indeterminate />
-            </div>
-          </template>
-        </VImg>
-      </div>
-
-      <div class="flex h-full flex-col px-5 px-md-8 pb-10">
-        <div class="my-2 h-full gap-5 flex flex-col">
-          <div class="flex mt-5 align-center gap-3">
-            <VChip class="my-5x">
-              {{ num }}
-            </VChip>
-
-            <VSpacer />
-
-            <VBtn color="grey-lighten-3" icon variant="outlined" @click.stop="emit('clickLink')">
-              <VIcon color="black" icon="i-mdi:open-in-new" />
-            </vbtn>
-
-            <VBtn color="grey-lighten-3" icon variant="outlined" @click.stop="emit('unfavorite')">
-              <VIcon color="primary" icon="i-mdi:heart" />
-            </vbtn>
-          </div>
-
-          <div class="flex flex-col">
-            <h3 class="text-2xl font-semibold blog-title" v-html="title" />
-          </div>
-
-          <p class="mt-5x truncate-lines-2 text-lg" v-html="alt" />
+            height="320"
+          >
+            <template #placeholder>
+              <div class="flex h-full items-center justify-center">
+                <VProgressCircular color="primary" indeterminate />
+              </div>
+            </template>
+          </VImg>
         </div>
-      </div>
-    </VCard>
+
+        <div class="flex h-full flex-col px-5 px-md-8 pb-10">
+          <div class="my-2 h-full gap-5 flex flex-col">
+            <div class="flex mt-5 align-center gap-3 h-12">
+              <VChip class="my-5x">
+                {{ num }}
+              </VChip>
+
+              <VSpacer />
+
+              <VFadeTransition>
+                <div v-show="isHovering" class="flex gap-3">
+                  <VBtn
+                    class="rounded-lg" color="grey-lighten-3" icon="i-mdi:heart" variant="outlined"
+                    @click.stop="emit('unfavorite')"
+                  >
+                    <VIcon color="primary" />
+                  </vbtn>
+
+                  <VBtn
+
+                    class="rounded-lg" color="grey-lighten-3" icon="i-mdi:open-in-new" variant="outlined"
+                    @click.stop="emit('clickLink')"
+                  >
+                    <VIcon color="black" />
+                  </vbtn>
+                </div>
+              </VFadeTransition>
+            </div>
+
+            <div class="flex flex-col">
+              <h3 class="text-2xl font-semibold blog-title" v-html="title" />
+            </div>
+
+            <p class="mt-5x truncate-lines-2 text-lg" v-html="alt" />
+          </div>
+        </div>
+      </VCard>
+    </VHover>
   </div>
 </template>
 
