732103a2e7107337c73281dca0a067a4e17df99d
santiagoaloi <santiagoaloi@gmail.com>
2024-04-21 03:17:54 +0200

    add keep-alive to cache views and layouts

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,16 @@
         max-width="1450"
       >
         <!-- This handles layouts only -->
-        <VFadeTransition hide-on-leave>
-          <RouterView />
-        </VFadeTransition>
+        <RouterView v-slot="{ Component }">
+          <template v-if="Component">
+            <!-- Cache views to enhance navigation speed across layouts and pages,
+              preserving the user's place in the comic when returning to the homepage
+              and the scroll position  -->
+            <KeepAlive>
+              <component :is="Component" />
+            </KeepAlive>
+          </template>
+        </RouterView>
       </VSheet>
     </AuthProvider>
     <!-- Login Dialog can be called from anywhere -->
