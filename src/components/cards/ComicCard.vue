6483f0ef63b2ac8dbe188a330c133d345bf8f095
santiagoaloi <santiagoaloi@gmail.com>
2024-04-21 03:48:36 +0200

    add favoritesViewType functionality

--- src/components/cards/ComicCard.vue
+++ src/components/cards/ComicCard.vue
@@ -20,6 +20,10 @@ defineProps({
     type: Number,
   },
 
+  viewType: {
+    type: Number,
+    default: 0,
+  },
 })
 
 const emit = defineEmits('unfavorite', 'clickLink')
@@ -30,59 +34,61 @@ const emit = defineEmits('unfavorite', 'clickLink')
     <VHover v-slot="{ isHovering, props }">
       <VCard
         v-bind="props"
-        class="active-push-down rounded-lg flex flex-col h-full card-color"
+        class="rounded-lg flex flex-col h-full card-color"
       >
-        <div>
-          <VImg
-            :src="img"
-            class="w-full prevent-select image-hover-scalex bg-background border-b"
-
-            height="320"
-          >
-            <template #placeholder>
-              <div class="flex h-full items-center justify-center">
-                <VProgressCircular color="primary" indeterminate />
-              </div>
-            </template>
-          </VImg>
-        </div>
-
-        <div class="flex h-full flex-col px-5 px-md-8 pb-10">
-          <div class="my-2 h-full gap-5 flex flex-col">
-            <div class="flex mt-5 align-center gap-3 h-12">
-              <VChip class="my-5x">
-                {{ num }}
-              </VChip>
-
-              <VSpacer />
-
-              <VFadeTransition>
-                <div v-show="isHovering" class="flex gap-3">
-                  <VBtn
-                    class="rounded-lg" color="grey-lighten-3" icon="i-mdi:heart" variant="outlined"
-                    @click.stop="emit('unfavorite')"
-                  >
-                    <VIcon color="primary" />
-                  </vbtn>
-
-                  <VBtn
-
-                    class="rounded-lg" color="grey-lighten-3" icon="i-mdi:open-in-new" variant="outlined"
-                    @click.stop="emit('clickLink')"
-                  >
-                    <VIcon color="black" />
-                  </vbtn>
+        <VRow>
+          <VCol :cols="viewType ? 12 : 6">
+            <VImg
+              :class="viewType ? 'border-b' : 'border-r'"
+              :src="img"
+              class="w-full prevent-select image-hover-scalex bg-background"
+              height="320"
+            >
+              <template #placeholder>
+                <div class="flex h-full items-center justify-center">
+                  <VProgressCircular color="primary" indeterminate />
                 </div>
-              </VFadeTransition>
-            </div>
+              </template>
+            </VImg>
+          </VCol>
 
-            <div class="flex flex-col">
-              <h3 class="text-2xl font-semibold blog-title" v-html="title" />
-            </div>
+          <VCol>
+            <div class="flex h-full flex-col px-5 px-md-8 pb-10">
+              <div class="my-2 h-full gap-5 flex flex-col">
+                <div class="flex mt-5 align-center gap-3 h-12">
+                  {{ num }}
 
-            <p class="mt-5x truncate-lines-2 text-lg" v-html="alt" />
-          </div>
-        </div>
+                  <VSpacer />
+
+                  <VFadeTransition>
+                    <div v-show="isHovering" class="flex gap-3">
+                      <VBtn
+                        class="rounded-lg" color="grey-lighten-3" icon="i-mdi:heart" variant="outlined"
+                        @click.stop="emit('unfavorite')"
+                      >
+                        <VIcon color="primary" />
+                      </vbtn>
+
+                      <VBtn
+
+                        class="rounded-lg" color="grey-lighten-3" icon="i-mdi:open-in-new" variant="outlined"
+                        @click.stop="emit('clickLink')"
+                      >
+                        <VIcon color="black" />
+                      </vbtn>
+                    </div>
+                  </VFadeTransition>
+                </div>
+
+                <div class="flex flex-col">
+                  <h3 class="text-2xl font-semibold blog-title" v-html="title" />
+                </div>
+
+                <p class="mt-5x truncate-lines-2 text-lg" v-html="alt" />
+              </div>
+            </div>
+          </VCol>
+        </VRow>
       </VCard>
     </VHover>
   </div>
