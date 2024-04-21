ed0958dd277545c4559fd8d0df7faf181a11a75b
santiagoaloi <santiagoaloi@gmail.com>
2024-04-21 05:21:07 +0200

    add themeSwitch component

---
+++ src/components/theme/ThemeSwitch.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useThemeStore } from '@/stores/theme'
+
+const themeStore = useThemeStore()
+</script>
+
+<template>
+  <VBtn
+
+    :icon="themeStore.isDark ? 'i-mdi:lightbulb-on' : 'i-mdi:lightbulb-outline'"
+    class="rounded-lg border-0"
+    color="link" rounded="lg"
+    variant="text"
+    @click="themeStore.themeToggle()"
+  />
+</template>
