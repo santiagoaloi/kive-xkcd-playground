775df8c34d3106fad78ef55d1a609ab4e5adaba1
santiagoaloi <santiagoaloi@gmail.com>
2024-04-21 03:18:54 +0200

    add action bar buttons to favorites page

--- src/pages/secure/_profile/Profile/Favorites.vue
+++ src/pages/secure/_profile/Profile/Favorites.vue
@@ -1,13 +1,41 @@
 <script setup>
-import { useUserProfileStore } from '@/stores/user-profile'
-
 import { useComicsStore } from '@/stores/comics'
+import { useUserProfileStore } from '@/stores/user-profile'
 
 const profileStore = useUserProfileStore()
 const comicsStore = useComicsStore()
 </script>
 
 <template>
+  <VSheet class="sticky top-20 align-center" color="background" height="80" style="z-index:1">
+    <div class="flex flex-col h-100 w-100 align-center justify-center">
+      <div class="w-100 flex align-center gap-3">
+        <VSpacer />
+
+        <SettingsButton class="mr-1" height="40" @click="profileStore.removeAllFavorites()">
+          Remove all favorites
+        </SettingsButton>
+
+        <VBtnToggle
+          v-model="profileStore.favoritesViewType"
+          class=" rounded-lg"
+          density="compact"
+          divided
+          mandatory
+          style="height: 40px"
+          variant="outlined"
+        >
+          <VBtn>
+            <VIcon icon="i-mdi-format-list-bulleted-square" />
+          </VBtn>
+          <VBtn>
+            <VIcon icon="i-mdi-dots-grid" />
+          </VBtn>
+        </VBtnToggle>
+      </div>
+    </div>
+  </VSheet>
+
   <VContainer v-if="!profileStore.profile.favorites.length" class="h-100">
     <div class="align-center justify-center flex flex-col mt-15">
       <DuoNoresults size="190" />
